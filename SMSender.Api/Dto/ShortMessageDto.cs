13b6793a532f7214b2aaf0e0c1fbb6b31a9918d5
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-06-27 11:58:18 +0400

    fix: return id

--- SMSender.Api/Dto/ShortMessageDto.cs
+++ SMSender.Api/Dto/ShortMessageDto.cs
@@ -4,6 +4,7 @@ namespace SMSender.Api.Dto
 {
     public class ShortMessageDto
     {
+        public string Id { get; set; }
         public string From { get; set; }
         public IEnumerable<string> To { get; set; }
         public string Content { get; set; }
